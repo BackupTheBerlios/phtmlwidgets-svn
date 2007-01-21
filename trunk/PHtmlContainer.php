0832bf5b99384b8501423187286e6f85c6bea24c
trinculescu <trinculescu@97ba20cb-7a27-0410-98d1-da3ac83517d7>
2007-01-21 07:00:24 +0000

    more logic to saveHTML

--- trunk/PHtmlContainer.php
+++ trunk/PHtmlContainer.php
@@ -2,20 +2,30 @@
 /* $Id$ */
 
 class PHtmlContainer {
+	protected $lang;
 	protected $dom;
 	protected $elements;
 
-	public function __construct($version = '1.0', $iso = 'iso-8859-1') {
-		$this->dom = new DOMDocument($version, $iso);
+	public function __construct($xmlns = 'http://www.w3.org/1999/xhtml', $lang = 'en') {
+		$this->dom = new DOMDocument();
+		$this->xmlns = $xmlns;
+		$this->lang = $lang;
 	}
 
 	public function & createElement($name, $value = null) {
 		return $this->dom->createElement($name, $value);
 	}
 
+	public function saveXML() {
+		$this->saveHTML();
+	}
+
 	public function saveHTML() {
-		$this->dom->appendChild($this->createElement('html'));
-		return $this->dom->saveHTML();
+		$this->dom->appendChild($this->dom->createComment('DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-tranitional.dtd"'));
+		$this->dom->appendChild($html = $this->dom->createElementNS($this->xmlns, 'html'));
+		$html->setAttribute('xml:lang', $this->lang);
+		$html->setAttribute('lang', $this->lang);
+		return $this->dom->saveXML();
 	}
 
 	public function __toString() {
