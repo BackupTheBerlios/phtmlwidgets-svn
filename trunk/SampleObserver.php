21a35818ab23cebc11aa813b24e78c67a746c8f0
trinculescu <trinculescu@97ba20cb-7a27-0410-98d1-da3ac83517d7>
2007-02-03 15:10:59 +0000

    modified SampleObserver

--- trunk/SampleObserver.php
+++ trunk/SampleObserver.php
@@ -4,6 +4,10 @@
 class SampleObserver implements SplObserver {
 	public function update(SplSubject $s) {
 		$cell = $s->getCell(2, 2);
+		$spans = $cell->getElementsByTagName('span');
+		foreach($spans as $span) {
+			$cell->removeChild($span);
+		}
 		$cell->appendChild(new DOMElement('span', ' :: Observed ::'));
 	}
 }
